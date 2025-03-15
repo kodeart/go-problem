e075754eb7055121cda62e5fd9ed243ef0628689
kodeart <kodeart@users.noreply.github.com>
2025-03-15 10:04:00 +0100

    chore: cleanup

--- problem.go
+++ problem.go
@@ -1,151 +1,83 @@
-// Copyright (c) 2024 Mihail Binev
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
-
 package problem
-
-import (
-	"encoding/json"
-	"fmt"
-	"reflect"
-	"strconv"
-)
 
 // Problem is a struct that represents a problem instance
 // as defined in RFC-9457 (https://tools.ietf.org/html/rfc9457).
-// All fields are optional.
 type Problem struct {
-	Status     int            `json:"status,omitempty"`
-	Instance   string         `json:"instance,omitempty"`
-	Detail     string         `json:"detail,omitempty"`
-	Title      string         `json:"title,omitempty"`
-	Type       string         `json:"type,omitempty"`
-	Extensions map[string]any `json:"-"`
+    Status     int            `json:"status,omitempty" xml:"status,omitempty"`
+    Instance   string         `json:"instance,omitempty" xml:"instance,omitempty"`
+    Detail     string         `json:"detail,omitempty" xml:"detail,omitempty"`
+    Title      string         `json:"title,omitempty" xml:"title,omitempty"`
+    Type       string         `json:"type,omitempty" xml:"type,omitempty"`
+    Extensions map[string]any `json:"-" xml:"-"`
 }
 
 // New returns a new Problem instance.
 func New() *Problem {
-	return new(Problem)
+    return &Problem{Extensions: make(map[string]any)}
 }
 
-// WithStatus sets the status code.
+// WithStatus sets the status code value.
 func (p *Problem) WithStatus(v int) *Problem {
-	p.Status = v
-	return p
+    p.Status = v
+    return p
 }
 
-// WithInstance sets the instance URI.
+// WithInstance sets the instance URI value.
 func (p *Problem) WithInstance(v string) *Problem {
-	p.Instance = v
-	return p
+    p.Instance = v
+    return p
 }
 
-// WithDetail sets the problem detail.
+// WithDetail sets the problem detail value.
 func (p *Problem) WithDetail(v string) *Problem {
-	p.Detail = v
-	return p
+    p.Detail = v
+    return p
 }
 
-// WithTitle sets the problem title.
+// WithTitle sets the problem title value.
 func (p *Problem) WithTitle(v string) *Problem {
-	p.Title = v
-	return p
+    p.Title = v
+    return p
 }
 
 // WithType sets the problem type.
 func (p *Problem) WithType(v string) *Problem {
-	p.Type = v
-	return p
+    p.Type = v
+    return p
 }
 
 // WithExtension adds key:value pairs to internal Extensions map.
 // When JSON serialization is performed, these pairs are
-// included in the JSON response as key:value to the final response.
+// included in the final JSON response as additional values.
 func (p *Problem) WithExtension(key string, val any) *Problem {
-	if p.Extensions == nil {
-		p.Extensions = make(map[string]any)
-	}
-	p.Extensions[key] = val
-	return p
+    if p.Extensions == nil {
+        p.Extensions = make(map[string]any)
+    }
+    p.Extensions[key] = val
+    return p
 }
 
 // WithoutExtension removes a key from internal Extensions map.
-// If the key does not exist, it does nothing.
+// If key is nil or there is no such element, WithoutExtension is a no-op.
 func (p *Problem) WithoutExtension(key string) *Problem {
-	delete(p.Extensions, key)
-	return p
+    delete(p.Extensions, key)
+    return p
 }
 
-// MarshalJSON implements json.Marshaler interface to serialize
-// the Problem instance into RFC-9457 JSON format.
-func (p Problem) MarshalJSON() ([]byte, error) {
-	m := map[string]any{}
-	if p.Status > 0 {
-		m["status"] = p.Status
-	}
-	if p.Instance != "" {
-		m["instance"] = p.Instance
-	}
-	if p.Detail != "" {
-		m["detail"] = p.Detail
-	}
-	if p.Title != "" {
-		m["title"] = p.Title
-	}
-	if p.Type != "" {
-		m["type"] = p.Type
-	}
-	for k, v := range p.Extensions {
-		m[k] = v
-	}
-	return json.Marshal(m)
-}
-
-// UnmarshalJSON implements json.Unmarshaler interface to unserialize
-// the JSON string into Problem structure as RFC-9457 implementation.
-// [IMPORTANT]: built-in json.Unmarshaler converts numeric values
-// to float64, so we need to convert status code back to int.
-// The extension values are not converted, but are available.
-func (p *Problem) UnmarshalJSON(data []byte) error {
-	var (
-		m   map[string]any
-		err error
-	)
-	if err = json.Unmarshal(data, &m); err != nil {
-		return err
-	}
-	if status, ok := m["status"]; ok {
-		if p.Status, err = p.toInt(status); err != nil {
-			return err
-		}
-	}
-	// get the values, cleanup and set the extensions (if any)
-	p.Extensions = make(map[string]any)
-	p.Instance, _ = m["instance"].(string)
-	p.Detail, _ = m["detail"].(string)
-	p.Title, _ = m["title"].(string)
-	p.Type, _ = m["type"].(string)
-	for _, f := range []string{"status", "instance", "detail", "title", "type"} {
-		delete(m, f)
-	}
-	for k, v := range m {
-		p.Extensions[k] = v
-	}
-	return nil
-}
-
-// toInt converts various numeric types into int.
-func (p Problem) toInt(value any) (int, error) {
-	switch v := reflect.ValueOf(value); v.Kind() {
-	case reflect.String:
-		i, err := strconv.Atoi(v.String())
-		if err != nil {
-			return 0, fmt.Errorf("invalid status type: %v", v.Kind())
-		}
-		return i, nil
-	case reflect.Float32, reflect.Float64:
-		return int(v.Float()), nil
-	default:
-		return 0, fmt.Errorf("invalid status type: %T", value)
-	}
+// GetExtension retrieves a value from Extensions map.
+// If there is no such element, nil is returned.
+// If you know the type of the value, you can assert it.
+//
+// Examples:
+// p := problem.New().WithExtension("key", "value")
+//
+// v := p.GetExtension("key")
+// s := p.GetExtension("key".(string)
+// i := p.GetExtension("key").(int)
+// b := p.GetExtension("key").(bool)
+func (p *Problem) GetExtension(key string) any {
+    if v, ok := (p.Extensions)[key]; ok {
+        return v
+    }
+    return nil
 }
